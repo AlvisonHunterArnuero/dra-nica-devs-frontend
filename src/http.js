d8d696909f78c5aaa0dd2382c309a3f632d426d2
Alvison Hunter <alvison.hunter@reeftechnology.com>
2021-10-25 15:00:43 -0600

    added new heroku app url

--- src/http.js
+++ src/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
 //  baseURL: "http://localhost:1337",
-baseURL: "https://dra-strapi-img.herokuapp.com/",
+baseURL: "https://sheltered-basin-12699.herokuapp.com/",
   headers: {
     "Content-type": "application/json",
   },
